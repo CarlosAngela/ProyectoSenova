2d27827a5f289b5b424f76bfc053e76c835a310a
unknown <Sala300@TUNDFPCGE10F352.FormacionSena.RED>
2019-08-06 17:18:27 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ProyectoSenova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            int puntaje = 0;
+            //Proyecto miproyecto = new Proyecto();
+            //Proyecto miproyecto2 = new Proyecto();
+            //Proyecto miproyecto3 = new Proyecto();
+            //Proyecto miproyecto4 = new Proyecto("Realidad aumentada", 12540, "Sistemas", 10);
+            //Proyecto miproyecto5 = new Proyecto("Talento Humano", 5);
+
+            //miproyecto3.IngresarProyecto();
+            //miproyecto3.ImprimirProyecto();
+            //miproyecto4.ImprimirProyecto();
+            //miproyecto5.ImprimirProyecto();
+
+
+            Proyecto mi1 = new Proyecto();
+            mi1.IngresarProyecto();
+
+            //miproyecto.Area = "sistemas";
+            //miproyecto.Codigo = 12000;
+            //miproyecto.Duracion = 13;
+            //miproyecto.Nombre = "Desarrollo internet de las cosasa IDT";
+
+
+
+
+
+            //miproyecto2.Area = "Redes";
+            //miproyecto2.Codigo = 10000;
+            //miproyecto2.Duracion = 14;
+            //miproyecto2.Nombre = "Desarrollo arduino";
+
+
+            //Console.WriteLine($"Area{miproyecto.Area}");
+            //Console.WriteLine($"Nombre:{miproyecto.Nombre}");
+            //Console.WriteLine($"Codigo:{miproyecto.Codigo}");
+            //Console.WriteLine($"Duracion:{miproyecto.Duracion}");
+
+            //Console.ReadLine();
+
+            //Console.WriteLine($"Area{miproyecto2.Area}");
+            //Console.WriteLine($"Nombre:{miproyecto2.Nombre}");
+            //Console.WriteLine($"Codigo:{miproyecto2.Codigo}");
+            //Console.WriteLine($"Duracion:{miproyecto2.Duracion}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Proyecto.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoSenova
+{
+    class Proyecto
+    {
+        #region Atributos 
+        #endregion
+        #region propiedades
+        #endregion
+
+
+
+        public string Nombre { get; set; }
+        public double Codigo{ get; set; }
+        public string Area { get; set; }
+        public int Duracion { get; set; }
+        #region Constructores
+        public Proyecto(string nombre,double codigo,string area,int duracion)
+        {
+            Nombre = nombre;
+            Codigo = codigo;
+            Area = area;
+            Duracion = duracion;
+
+        }
+        public Proyecto()
+        {
+
+        }
+        public Proyecto( string area,int duracion)
+        {
+            Area = area;
+            Duracion = duracion;
+
+        }
+        #endregion
+        #region   Metodos 
+        public void IngresarProyecto()
+        {
+            Console.WriteLine("Ingrese el nombre del proyecto");
+            Nombre = Console.ReadLine();
+
+
+            Console.WriteLine("Ingrese el codigo");
+            Codigo = Double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Ingese el area");
+            Area = Console.ReadLine();
+
+
+
+            Console.WriteLine("Ingrese Duracion");
+            Duracion = int.Parse(Console.ReadLine());
+        }
+        public void ImprimirProyecto()
+        {
+            Console.WriteLine($"el nombre es: {Nombre}");
+            Console.WriteLine($"el codigo es: {Codigo }");
+            Console.WriteLine($"el Area es. {Area }");
+            Console.WriteLine($"la  Duracion es {Duracion}");
+        }
+        #endregion
+    }
+
+}
+
